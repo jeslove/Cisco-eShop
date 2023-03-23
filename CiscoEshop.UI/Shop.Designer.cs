40491ccdcd4c9b6a2fa0938d7d51140e6a8aba45
JANET-DJANGO <so_django@pm.me>
2023-03-23 17:11:19 +0000

    completed objective

--- CiscoEshop.UI/Shop.Designer.cs
+++ CiscoEshop.UI/Shop.Designer.cs
@@ -30,14 +30,19 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.inventoryGrid = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.inventorySearchTxtbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inventorySearchBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.inventoryGrid)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(405, 63);
+            this.label1.Location = new System.Drawing.Point(405, 50);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(239, 34);
             this.label1.TabIndex = 0;
@@ -56,11 +61,50 @@
             this.inventoryGrid.Size = new System.Drawing.Size(853, 362);
             this.inventoryGrid.TabIndex = 1;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSalmon;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.inventorySearchBtn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.inventorySearchTxtbox);
+            this.panel1.Location = new System.Drawing.Point(74, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(865, 54);
+            this.panel1.TabIndex = 2;
+            // 
+            // inventorySearchTxtbox
+            // 
+            this.inventorySearchTxtbox.Location = new System.Drawing.Point(130, 10);
+            this.inventorySearchTxtbox.Name = "inventorySearchTxtbox";
+            this.inventorySearchTxtbox.Size = new System.Drawing.Size(192, 26);
+            this.inventorySearchTxtbox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Item name:";
+            // 
+            // inventorySearchBtn
+            // 
+            this.inventorySearchBtn.Location = new System.Drawing.Point(351, 10);
+            this.inventorySearchBtn.Name = "inventorySearchBtn";
+            this.inventorySearchBtn.Size = new System.Drawing.Size(75, 34);
+            this.inventorySearchBtn.TabIndex = 2;
+            this.inventorySearchBtn.Text = "Search";
+            this.inventorySearchBtn.UseVisualStyleBackColor = true;
+            this.inventorySearchBtn.Click += new System.EventHandler(this.inventorySearchBtn_Click);
+            // 
             // Shop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1029, 570);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.inventoryGrid);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -70,6 +114,8 @@
             this.Text = "Shop";
             this.Load += new System.EventHandler(this.Shop_Load);
             ((System.ComponentModel.ISupportInitialize)(this.inventoryGrid)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,5 +125,9 @@
 
         private Label label1;
         private DataGridView inventoryGrid;
+        private Panel panel1;
+        private Button inventorySearchBtn;
+        private Label label2;
+        private TextBox inventorySearchTxtbox;
     }
 }
--- CiscoEshop.UI/Shop.cs
+++ CiscoEshop.UI/Shop.cs
@@ -13,21 +13,37 @@ namespace CiscoEshop.UI
 {
     public partial class Shop : Form
     {
+        public readonly List<Router> _items ;
         public Shop()
         {
             InitializeComponent();
-            var items = new List<Router>
+            _items = new List<Router>
             {
                 new Router {Name = "CISCO RI4MX",Price=23.5,BuiltInFirewall=true },
                 new Router {Name = "CISCO RI4MX Version 1",Price=182,BuiltInFirewall=true },
                 new Router {Name = "CISCO AMB6X",Price=210,BuiltInFirewall=false },
                 new Router {Name = "CISCO RA6mX",Price=103.5,BuiltInFirewall=false }
-            };
-            inventoryGrid.DataSource = items.OrderBy(a => a.Name).ToList();
+            }.OrderBy(a => a.Name).ToList();
+            inventoryGrid.DataSource = _items;
         }
 
         private void Shop_Load(object sender, EventArgs e)
         {
+        }
+
+        private void inventorySearchBtn_Click(object sender, EventArgs e)
+        {
+            if(inventorySearchTxtbox.Text.Length>1)
+            {
+                string searchText = inventorySearchTxtbox.Text;
+                inventoryGrid.DataSource = _items.Where(a => a.Name.Contains(searchText,StringComparison.InvariantCultureIgnoreCase)).OrderBy(e=>e.Name).ToList();
+            }
+            else
+            {
+                inventoryGrid.DataSource = _items;
+                MessageBox.Show("Please enter some text first", "Search Results", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
         }
     }
 }
